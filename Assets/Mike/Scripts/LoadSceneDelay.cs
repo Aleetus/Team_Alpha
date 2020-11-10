e758604ea81a9737ce936a7f896e405ba9c84a93
Michael Wiggins <random-mike@hotmail.com>
2020-11-10 13:58:18 +0000

    stuff

---
+++ Assets/Mike/Scripts/LoadSceneDelay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneDelay : MonoBehaviour
+{
+    public float delay = 440;
+    public string loadLevel;
+
+
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(loadLevel);
+    }
+}
---
+++ Assets/Mike/Scripts/LoadSceneOnTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadSceneOnTrigger : MonoBehaviour
+{
+
+    public string loadScene;
+
+    // Detect if something enters the Trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(loadScene);
+        }
+    }
+}
