89233978bbaf49a92641f4eeed8e39880fea5d12
Lucas <58072322+Just-want-to-make-an-account@users.noreply.github.com>
2020-11-14 13:15:34 +0000

    Health system
    Slider for health display
    Enemy that damages on collsion
    
    Also made some changes to the camera, follows player better

---
+++ Assets/Lucas/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float hitPoints;
+    public float maxHitPoints = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitPoints = maxHitPoints;
+    }
+
+    void TakeHit(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Lucas/Scripts/PlayerLife.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour
+{
+
+    public Slider healthSlider;
+    public float maxHealth;
+    public static float currentHealth;
+
+    public float damage;
+
+    void Start()
+    {
+        currentHealth = maxHealth; // The players health is set to the max health at the start.
+    }
+
+    void Update()
+    {
+        healthSlider.value = currentHealth; // Here we set the player health on the UI.
+
+        if (Input.GetKey(KeyCode.H)) // This causes damage.
+        {
+            TakeDamage();
+        }
+
+        if (currentHealth <= 0) // If the player health is 0 they die.
+        {
+            Death();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            TakeDamage();
+        }
+    }
+
+    void TakeDamage() // This is what removes helath from the player.
+    {
+        currentHealth -= damage;
+    }
+
+    void Death()  // This Kills the player.
+    {
+        gameObject.SetActive(false);
+        healthSlider.value = 0;
+        //Destroy(gameObject);
+    }
+    
+}
