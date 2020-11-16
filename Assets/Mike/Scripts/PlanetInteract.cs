1a67dfb525572cbf7c6a4f1efc9c457a6d164426
Michael Wiggins <random-mike@hotmail.com>
2020-11-16 11:05:35 +0000

    testing planet interaction

---
+++ Assets/Mike/Scripts/PlanetInteract.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlanetInteract : MonoBehaviour
+{
+    public string loadScene;
+
+    // Detect if something enters the Trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            print ("collider detected");
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                print ("space is being pressed");
+                SceneManager.LoadScene(loadScene);
+            }     
+        }
+    }
+}
