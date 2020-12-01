1de00a0511a16689093fb92ef54e1e6c1c288754
Michael Wiggins <random-mike@hotmail.com>
2020-12-01 11:59:57 +0000

    Text display on trigger added

---
+++ Assets/Mike/Scripts/TextPopUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextPopUp : MonoBehaviour
+{
+    public GameObject UIObject;
+    public GameObject Trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            UIObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        UIObject.SetActive(false);
+        Destroy(Trigger);
+    }
+
+}
