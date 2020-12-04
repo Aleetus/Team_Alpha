2afef76a5eba2a5bd86cf6fd4805aa359191a098
Lucas <58072322+Just-want-to-make-an-account@users.noreply.github.com>
2020-12-04 16:53:39 +0000

    Fixes to all enemy prefabs / Attempt at enemy shoot system / shoot and jump audio also added to player prefab

---
+++ Assets/Lucas/Scripts/Enemy Scripts/EnemyProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float dieTime, damage;
+
+
+    void Start()
+    {
+        StartCoroutine(CountDownTimer());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerLife.currentHealth -= damage;
+        Die();
+    }
+
+    IEnumerator CountDownTimer()
+    {
+        yield return new WaitForSeconds(dieTime);
+        Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Lucas/Scripts/Enemy Scripts/EnemyShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public float range, timeBtwShots, shootSpeed;
+    private float distanceToPlayer;
+    public Transform targetPlayer;
+    public Transform shootPos;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.find("PlayerCharacter")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector2.Distance(transform.position, targetPlayer.position);
+
+        if (distanceToPlayer <= range)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(timeBtwShots);
+        GameObject newProjectile = Instantiate(projectile, shootPos.position, Quaternion.identity);
+        newProjectile.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * Time.fixedDeltaTime, 0f);
+    }
+}
