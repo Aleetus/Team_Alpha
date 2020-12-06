84aec44498634163d1e24d0699155c52618597eb
Michael Wiggins <random-mike@hotmail.com>
2020-12-06 15:32:45 +0000

    Level one complete

--- Assets/ShipTakeOff.cs
+++ Assets/ShipTakeOff.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ShipTakeOff : MonoBehaviour
 {
+    public GameObject enemyShip;
+    public float speed;
+    public bool isFlying;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFlying)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isFlying = true;
+        }
+    }
+
 }
