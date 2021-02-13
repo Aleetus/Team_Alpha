45f3bcaf59e8c7334a727498c355be71a26aa4a1
Michael Wiggins <random-mike@hotmail.com>
2021-02-13 11:46:32 +0000

    restart button added to pause menu
    also fixed level two background

---
+++ Assets/Mike/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneToLoad;
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        canvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
