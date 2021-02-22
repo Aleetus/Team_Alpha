a3c83e03f3e854520a215e4c6b0f406c8d0e0c99
Lucas <58072322+Just-want-to-make-an-account@users.noreply.github.com>
2021-02-22 13:22:34 +0000

    Falling / Crumbling Platforms now respawn at original position

--- Assets/Lucas/Scripts/Falling_Platform.cs
+++ Assets/Lucas/Scripts/Falling_Platform.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Falling_Platform : MonoBehaviour
 {
-
     Rigidbody2D rb;
 
+    public Vector2 respawnPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-      
+        respawnPosition = transform.position;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -21,12 +20,25 @@ public class Falling_Platform : MonoBehaviour
         if (collision.gameObject.tag.Equals("Player"))
         {
             Invoke("PlatformDrop", 0.5f);
-            Destroy (gameObject, 1f);
+            Invoke("PlatNotActive", 1f);
+            Invoke("Respawn", 5f);
         }
     }
 
     void PlatformDrop()
     {
         rb.isKinematic = false;
+    }
+
+    void PlatNotActive()
+    {
+        gameObject.SetActive(false);
+        rb.isKinematic = true;
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPosition;
+        gameObject.SetActive(true);
     }
 }
--- Assets/Lucas/Scripts/Melting_Platform.cs
+++ Assets/Lucas/Scripts/Melting_Platform.cs
@@ -6,25 +6,33 @@ public class Melting_Platform : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    public Vector2 respawnPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-
+        respawnPosition = transform.position;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
-            Destroy(gameObject, 1f);
+            Invoke("PlatNotActive", 1f);
+            Invoke("Respawn", 5f);
         }
     }
 
-    void PlatformDrop()
+    void PlatNotActive()
     {
-        rb.isKinematic = false;
+        gameObject.SetActive(false);
+        rb.isKinematic = true;
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPosition;
+        gameObject.SetActive(true);
     }
 }
