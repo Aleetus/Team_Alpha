bf4644f4cc6955d80bec9f6962a079f9c20aa303
Michael Wiggins <random-mike@hotmail.com>
2021-04-11 16:32:27 +0100

    Text pop ups added to LVL3 and more refinements

---
+++ Assets/Trigger_Load_Scene_Delayed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trigger_Load_Scene_Delayed : MonoBehaviour
+{
+    public float delay = 440;
+    public string loadLevel;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        void Start()
+        {
+            StartCoroutine(LoadLevelAfterDelay(delay));
+        }
+
+        IEnumerator LoadLevelAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(loadLevel);
+            }
+        }
+    }
+
+
+}
